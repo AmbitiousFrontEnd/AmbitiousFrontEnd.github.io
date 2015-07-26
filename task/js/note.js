98a47b2192da62094b8166ef07ba40823480a90a
xioaruo <tian.ruo@outlook.com>
2015-07-26 22:04:29 +0800

    添加markdow显示

--- task/js/note.js
+++ task/js/note.js
@@ -3,6 +3,9 @@ var note = {
         this.setSize();
         this.addclassify();
         this.addnote();
+        this.saveNote();
+        this.highLightCode();
+        this.preview();
     },
     element: {
         addbutton: document.getElementById('addbutton'),
@@ -71,10 +74,30 @@ var note = {
     addnote: function() {
         var create = document.getElementById('create');
         EventUtil.addHandler(create, 'click', function() {
-            var textarea = document.getElementById('textarea');
-            textarea.style.display = 'block';
-            concise.$('#rtitle').style.backgroundColor = '#FFF';
-            concise.$('#title').style.display = 'inline-block';
+            concise.removeClass(concise.$('#write'), 'none');
+            concise.addClass(concise.$('#write'), 'block');
+            concise.removeClass(concise.$('#show'), 'block');
+            concise.addClass(concise.$('#show'), 'none');
+        });
+    },
+    saveNote: function() {
+        var that = this;
+        var save = document.getElementById('save');
+        EventUtil.addHandler(save, 'click', function(){
+            concise.removeClass(concise.$('#show'), 'none');
+            concise.addClass(concise.$('#show'), 'block');
+            concise.removeClass(concise.$('#write'), 'block');
+            concise.addClass(concise.$('#write'), 'none');
+            var content = document.getElementById('textarea').value;
+            var right = document.getElementById('rtext');
+            var hCon = marked(content);
+            right.innerHTML = hCon;
+            that.highLightCode();
+            if(concise.hasClass(concise.$('#view'), 'block')) {
+                concise.removeClass(concise.$('#view'), 'block');
+                concise.addClass(concise.$('#view'), 'none');
+            }
+            concise.$('#ititle').innerHTML = concise.$('#title').value;
         });
     },
     showClassify: function(classify) {
@@ -85,6 +108,31 @@ var note = {
         a.innerHTML = classify;
         p.appendChild(a);
         note.element.classify.appendChild(p);
+    },
+    highLightCode: function() {
+        var codeBlock =document.querySelectorAll('pre code');
+        for(var i = codeBlock.length-1;i>=0;i--){
+            hljs.highlightBlock(codeBlock[i].parentNode);
+        }
+    },
+    preview: function() {
+        var that = this;
+        var view = document.getElementById('preview');
+        EventUtil.addHandler(view, 'click', function() {
+            concise.removeClass(concise.$('#view'), 'none');
+            concise.addClass(concise.$('#view'), 'block');
+            that.highLightCode();
+        });
+        EventUtil.addHandler(concise.$('#textarea'), 'keyup', function() {
+            if(concise.hasClass(concise.$('#view'), 'block')) {
+
+                var content = document.getElementById('textarea').value;
+                var right = document.getElementById('view');
+                var hCon = marked(content);
+                right.innerHTML = hCon;
+                that.highLightCode();
+            }
+        });
     }
 };
 note.inInt();
